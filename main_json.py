d4e7f74f6e7edd2e4253b0e4199e65017ea2eab4
feisalramdhan <feisalramdhan@gmail.com>
2022-11-21 17:27:12 +0700

    Add files via upload

---
+++ main_json.py
@@ -0,0 +1,22 @@
+import tornado.web
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.write("Hello World")
+
+class staticRequestHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render("Cars.json")
+		
+if __name__ == '__main__':
+	app = tornado.web.Application([
+		(r"/", basicRequestHandler),
+		(r"/7B2_RAR", staticRequestHandler)
+
+
+	])
+
+	app.listen(8881)
+	print("I'm listening on port 8881")
+	tornado.ioloop.IOLoop.current().start()
